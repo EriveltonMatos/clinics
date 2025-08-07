f40ac135810e8b46c437e7e164129d4984039faf
Erivelton Matos <eriveltonsdmatos@gmail.com>
2025-08-07 11:51:43 -0300

    adicionando metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/api/AuthContext";
 import { ThemeProvider } from "@/components/ThemeProvider";
-
+import ressonancia from "../assets/ressonancia.jpg";
 
 export const metadata: Metadata = {
   title: "Clínicas Unichristus",
   description: "Portal de clínicas escola Unichristus",
+  openGraph: {
+    title: "Clínicas Unichristus",
+    description: "Conheça o portal das clínicas escola da Unichristus. Atendimento, agendamento e muito mais.",
+    url: "https://clinicas.unichristus.edu.br", // Altere para o link real
+    siteName: "Clínicas Unichristus",
+    images: [
+      {
+        url: ressonancia.src, 
+        width: 1000,
+        height: 630,
+        alt: "Clínicas Unichristus",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
